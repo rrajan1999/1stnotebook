836e9719ba9c84e7e3cfcd0fd1ff8684a26778fa
rrajan1999 <155226686+rrajan1999@users.noreply.github.com>
2023-12-31 21:05:11 +0530

    Add files via upload

---
+++ 1stJupiterWork.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+#    # objectives:
+#    - Data Analysis
+#    - Data Visualisation
+#    - Machine learning 
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. Julia
+
+# Some of the commonly used libraries used by Data Scientists include:
+#  1. Numpy
+#  2. Pandas.
+#  3. Matplotlib
+#  4. Seaborn
+
+# |Data Science Tools|
+# |------------------|
+# |Jupiter Notebook  | 
+# | RStudio          |
+# | Spyder           |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+
+# In[4]:
+
+
+## This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[5]:
+
+
+## This will convert 200 minutes to hours by diving by 60
+200/60
+
+
+# ## Author
+# Rajan Yadav
+
+# In[ ]:
+
+
+
+
